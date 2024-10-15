12dadb603d075b54f6906da8f21f9f6459928965
Amelie Engelmaierová <amelieengelmaierova@eduroam151-144.nbk.vse.cz>
2024-10-15 12:16:52 +0200

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,10 @@ import '../index.css';
 
 export default function Footer() {
     return (
-        <div class="footer">This will always appear at the bottom of the page, but <strong>not fixed</strong>.</div>
+        <div class="footer">
+            <a href="https://www.linkedin.com/in/amelie-engelmaierov%C3%A1-a94b4a136/">LinkedIn</a>
+            <a href="mailto:ikoronka.work@gmail.com">mail</a>
+            <a href="https://github.com/ikoronka/">github</a>
+        </div>
     );
 }
