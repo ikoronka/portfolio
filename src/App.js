213e97458efea4c6cf344eae2f55b16acee7678f
Amelie Engelmaierová <amelie.engelmaier@gmail.com>
2025-02-21 14:28:19 +0100

    g

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './pages/About';
 import Footer from './components/Footer';
 import Navigation from './components/Navigation';
 import Holka from './images/holka_gimp.jpg';
+import Kytka from './images/kytka.jpg';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <About/>
       <Navigation/>
       <img src={Holka} alt="holka" width={300}/>
+      <img src={Kytka} alt="holka" width={300}/>
     </div>
   );
 }
