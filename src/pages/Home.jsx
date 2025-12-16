822acdb842d0aee6a40557e57a9108b9ecf9f606
Amelie Engelmaierová <amelie.engelmaier@gmail.com>
2025-12-16 16:08:19 +0100

    fix(descr): order

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import '../styles/index.css';
+import React from "react";
+import "../styles/index.css";
 import { ReactTyped } from "react-typed";
 
 export default function Home() {
-
-    return (
-      <div className='fullPage'>
-
-        <div id="centeredText">
-          <div>
-            <ReactTyped 
+  return (
+    <div className="fullPage">
+      <div id="centeredText">
+        <div>
+          <ReactTyped
             strings={[
-              "Hi, I'm <br> <span style='color: #FFAB00; font-weight: 600' >a teacher</span>",
               "Hi, I'm <br> <span style='color: #009CEA; font-weight: 600' >a dev</span>",
               "Hi, I'm <br> <span style='color: #74AF00; font-weight: 600' >a student</span>",
-              "Hi, I'm <br> <span style='color: #E22E57; font-weight: 600' >Amy</span>"
-            ]} 
+              "Hi, I'm <br> <span style='color: #FFAB00; font-weight: 600' >a coach</span>",
+              "Hi, I'm <br> <span style='color: #E22E57; font-weight: 600' >Amy</span>",
+            ]}
             typeSpeed={40}
-            className='titleText' />
-            <br />
-          </div>
+            className="titleText"
+          />
+          <br />
         </div>
       </div>
-    );
+    </div>
+  );
 }
